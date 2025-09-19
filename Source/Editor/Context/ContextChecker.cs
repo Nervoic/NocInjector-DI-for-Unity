bc8784b078b5dc913c4f48208141cbc5b7d8e337
Nervoic <nearveanee@gmail.com>
2025-09-19 16:35:03 +0300

    Hotfix v2.1
    Added a warning when there is no ContextManager on the stage

---
+++ Source/Editor/Context/ContextChecker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace NocInjector
+{
+    [InitializeOnLoad]
+    public class ContextChecker
+    {
+        static ContextChecker()
+        {
+            EditorApplication.hierarchyChanged += OnHierarchyChanged;
+        }
+
+        private static void OnHierarchyChanged()
+        {
+            var context = Object.FindAnyObjectByType<Context>();
+
+            if (context is null) return;
+            
+            var contextManager = Object.FindAnyObjectByType<ContextManager>();
+
+            if (contextManager is null)
+                Debug.LogWarning($"Add {nameof(ContextManager)} to the scene for NocInjector to work");
+            
+        }
+    }
+}
